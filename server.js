e5b3b125f5b1822174d9f680c7b94ea3c1343c4a
Rafael Makaha <rafael.makaha@gmail.com>
2019-08-05 17:16:11 -0300

    Changes Server.js configs.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,74 @@
 // Modules
 var express = require('express');
-var app = express();
 var http = require('http');
 var bodyParser = require('body-parser');
+
+const path = require('path');
+const passport = require('passport');
+const morgan = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+const session = require('express-session');
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
+
+
+//starts express
+var app = express();
+
+// okta configs
+var oktaClient = new okta.Client({
+    orgUrl: 'https://dev-543119.okta.com',
+    token: '00FWj8iu34A5192znyOHxEpQhrsIIj3ddxuQ4ssSkS'
+});
+
+const oidc = new ExpressOIDC({
+    issuer: "https://dev-543119.okta.com/oauth2/default",
+    client_id: "0oa129ra9cyIDY1hq357",
+    client_secret: "7gATp3gpz7XndgJhBT5BJXAmLT4IXopDeYMcgDO3",
+    redirect_uri: 'http://localhost:7676/app',
+    scope: "openid profile",
+    routes: {
+      login: {
+        path: "/login"
+      },
+      callback: {
+        path: "/app",
+        defaultRedirect: "/app"
+      }
+    }
+});
+
+app.use(session({
+    secret: 'asdf;lkjh3lkjh235l23h5l235kjh',
+    resave: true,
+    saveUninitialized: false
+}));
+
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+    if (!req.userinfo) {
+      return next();
+    }
+  
+    oktaClient.getUser(req.userinfo.sub)
+      .then(user => {
+        req.user = user;
+        res.locals.user = user;
+        next();
+      }).catch(err => {
+        next(err);
+      });
+  });
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
 
 // load config file
 var config = require('./app/config.json');
@@ -28,7 +94,7 @@ app.set('view engine', 'pug');
 //app.set('views', __dirname + '/public/views');
 
 // Routes
-require('./app/routes')(app);
+require('./app/routes')(loginRequired,app);
 
 // IO
 require('./app/io')(io);
