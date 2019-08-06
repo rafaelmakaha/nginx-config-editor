150f469f9b81269b08cc0d41b59a6b3a7109db27
Rafael Makaha <rafael.makaha@gmail.com>
2019-08-06 20:46:14 -0300

    Adds Passport.js file.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ app/passport.js
@@ -0,0 +1,30 @@
+const db = require('./database');
+const LocalStrategy = require('passport-local').Strategy;
+
+module.exports = function(passport) {
+    // serializer
+    passport.serializeUser(function (user, done){
+        done(null, user)
+    });
+
+    // deserializer
+    passport.deserializeUser(function (user, done) {
+        done(user);
+    })
+
+    // login
+    passport.use('local-login', new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function (req, username, password, done) {
+        if (username != db.username){
+            return done(null, false, req.flash('loginMessage', 'Usuário não encontrado.'));
+        }
+        if (password != db.password){
+            return done(null, false, req.flash('loginMessage', 'Senha incorreta.'));
+        }
+        return done(null, username);
+    }))
+}
